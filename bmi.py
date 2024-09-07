888ec02ac8b19c094bc9596852100d76c3d36554
tony <tony455448@gmail.com>
2024-09-07 16:24:59 +0800

    [新增] 新增輸入例外

--- bmi.py
+++ bmi.py
@@ -1,14 +1,17 @@
-heigh=float(input("請輸入身高,單位為(公分):"))
-weight=float(input("請輸入體重,單位為(公斤):"))
+try:
+    heigh=float(input("請輸入身高,單位為(公分):"))
+    weight=float(input("請輸入體重,單位為(公斤):"))
 
-bmi=round(weight/(heigh/100)**2,5)
-print(f"您的BMI是{bmi}")
+    bmi=round(weight/(heigh/100)**2,5)
+    print(f"您的BMI是{bmi}")
 
-if bmi<18.5:
-    print("太輕")
-elif 18.5<=bmi<25:
-    print("正常")
-elif 25<=bmi<30:
-    print("過重")
-else:
-    print("肥胖")
+    if bmi<18.5:
+        print("太輕")
+    elif 18.5<=bmi<25:
+       print("正常")
+    elif 25<=bmi<30:
+      print("過重")
+    else:
+       print("肥胖")
+except:
+    print("輸入錯誤")
