e5ac861d44c27010270e301e457260515ec1fa64
tony <tony455448@gmail.com>
2024-09-14 11:48:25 +0800

    [修改]  bmi 輸入 exception

--- bmi.py
+++ bmi.py
@@ -3,16 +3,22 @@ while True:
     try:
         heigh=float(input("請輸入身高,單位為(公分):"))
         break
-    except:
-        print("輸入錯誤")
+    except ValueError:
+        print('輸入格式錯誤')
+        continue
+    except Exception as e:
+        print(f'輸入錯誤{heigh}')
         continue
   
   while True:
      try:
         weight=float(input("請輸入體重,單位為(公斤):"))
         break
-     except:
-        print("輸入錯誤")
+     except ValueError:
+        print('輸入格式錯誤')
+        continue
+     except Exception as e:
+        print(f'輸入錯誤{weight}')
         continue
     
   bmi=round(weight/(heigh/100)**2,5)
