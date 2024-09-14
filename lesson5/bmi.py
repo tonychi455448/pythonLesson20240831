da84c47db161e546544d361d69fff099b6cec1b4
tony <tony455448@gmail.com>
2024-09-14 14:44:21 +0800

    [新增] bmi pakage 寫法

--- lesson5/bmi.py
+++ lesson5/bmi.py
@@ -1,11 +1,11 @@
-import tools
+import widget
 
 #主程式
 while True:
-  HEIGH,WEIGH=tools.input_data()
-  BMI=tools.calculate_bmi(weight=WEIGH,heigh=HEIGH)
+  HEIGH,WEIGH=widget.input_data()
+  BMI=widget.calculate_bmi(weight=WEIGH,heigh=HEIGH)
   print(f"您的BMI是{BMI}")
-  print(tools.get_status(BMI))
+  print(widget.get_status(BMI))
 
   result=input("繼續?(y/n)")
   if result=="n" or result=="N":
