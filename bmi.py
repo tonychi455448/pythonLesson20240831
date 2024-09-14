c4a3f8e5efa2128edcae1c1ce242bdee00fd9026
tony <tony455448@gmail.com>
2024-09-14 11:32:48 +0800

    [新增] bmi while loop

--- bmi.py
+++ bmi.py
@@ -1,17 +1,33 @@
-try:
-    heigh=float(input("請輸入身高,單位為(公分):"))
-    weight=float(input("請輸入體重,單位為(公斤):"))
+while True:
+  while True:
+    try:
+        heigh=float(input("請輸入身高,單位為(公分):"))
+        break
+    except:
+        print("輸入錯誤")
+        continue
+  
+  while True:
+     try:
+        weight=float(input("請輸入體重,單位為(公斤):"))
+        break
+     except:
+        print("輸入錯誤")
+        continue
+    
+  bmi=round(weight/(heigh/100)**2,5)
+  print(f"您的BMI是{bmi}")
 
-    bmi=round(weight/(heigh/100)**2,5)
-    print(f"您的BMI是{bmi}")
+  if bmi<18.5:
+    print("太輕")
+  elif 18.5<=bmi<25:
+    print("正常")
+  elif 25<=bmi<30:
+    print("過重")
+  else:
+    print("肥胖")
 
-    if bmi<18.5:
-        print("太輕")
-    elif 18.5<=bmi<25:
-       print("正常")
-    elif 25<=bmi<30:
-      print("過重")
-    else:
-       print("肥胖")
-except:
-    print("輸入錯誤")
+  result=input("繼續?(y/n)")
+  if result=="n":
+    break
+print("程式結束")
