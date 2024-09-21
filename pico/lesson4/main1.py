a5bcd660d642b37b2eec4043de9dbeb491e1f946
tony <tony455448@gmail.com>
2024-09-21 13:50:17 +0800

    [新增] 課堂練習-pico led、超音波、紅外線

--- pico/lesson4/main1.py
+++ pico/lesson4/main1.py
@@ -1,1 +1,19 @@
-print("hello")
+from machine import Pin
+import time
+
+led=Pin("LED",Pin.OUT)
+status=False 
+# i=0
+while True:
+    if status==False:
+        status=True 
+        led.on()
+    else:
+        status=False  
+        led.off()
+    time.sleep(1)
+#     i+=1
+#     led.value(i%2)
+#     time.sleep(1)
+#     print(i%2)
+#led.on()    
