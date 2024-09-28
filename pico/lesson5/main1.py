6f3c3d33ab0673f17438099620efb19691a5f09c
tony <tony455448@gmail.com>
2024-09-28 10:22:42 +0800

    [新增] pico 內建溫度讀取

--- pico/lesson5/main1.py
+++ pico/lesson5/main1.py
@@ -1,1 +1,9 @@
-print("hello python")
+from machine import ADC
+import time
+
+while True:
+    adc = machine.ADC(4) # Connect to GP26, which is channel 0
+    temp1=adc.read_u16()
+    #temp1=temp1/65535
+    print(temp1)
+    time.sleep(1)
