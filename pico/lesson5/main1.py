fed184ef0ba396f51a2bc992ff9df9054b33ee12
tony <tony455448@gmail.com>
2024-09-28 10:36:41 +0800

    [修改] pico 內建溫度轉換

--- pico/lesson5/main1.py
+++ pico/lesson5/main1.py
@@ -3,7 +3,7 @@ import time
 
 while True:
     adc = machine.ADC(4) # Connect to GP26, which is channel 0
-    temp1=adc.read_u16()
-    #temp1=temp1/65535
+    temp1=adc.read_u16()/65535*3.3
+    temp1= 27 - (temp1 - 0.706)/0.001721
     print(temp1)
     time.sleep(1)
