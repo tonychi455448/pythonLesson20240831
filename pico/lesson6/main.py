e9465a9612928e82cbb1a362fc8244494cebca52
tony <tony455448@gmail.com>
2024-10-05 11:36:49 +0800

    [新增] 文件備註以及格式

---
+++ pico/lesson6/main.py
@@ -0,0 +1,52 @@
+#! usr/bin/micropython
+'''
+led->gpio15
+光敏電阻->gpio28
+可變電阻->gpio26
+內建溫度sensor->adc(4) adc最後1pin，共5pin
+'''
+
+import tools
+from machine import Timer,ADC,Pin,PWM,RTC
+def do_thing(t):
+    '''
+    :param t:Timer的實體 
+    處理溫度和光線
+    每2秒執行1次
+    '''
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(f'溫度:{temperature}')
+    light_value = adc_light.read_u16()
+    print(f'光線:{light_value}')
+    
+    
+def do_thing1(t):
+    '''
+    :param t:Timer的實體 
+    處理可變電阻
+    每1秒執行1次
+    '''
+    adc1 = ADC(Pin(26))
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty)    
+    print(f'可變電阻:{round(duty/65535*10)}')
+    
+def main():
+    t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+    t2 = Timer(period=1000, mode=Timer.PERIODIC, callback=do_thing1)
+    
+if __name__ == "__main__":
+    #pico_連結電腦時的寫法,要用connect
+    try:
+        #tools.connect()
+        pass
+    except RuntimeError as e:
+        print(f"{{e}")
+    else:
+        #sensor setup
+        adc = ADC(4) #內建溫度感測器
+        adc_light = ADC(Pin(28)) #光線感測器
+        pwm = PWM(Pin(15),freq=50) #可變電阻
+        conversion_factor = 3.3 / (65535) #電壓轉換率    
+        main()
