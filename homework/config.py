b609998c799433725b475e99efca1a29f852abc2
tony <tony455448@gmail.com>
2024-10-05 10:20:32 +0800

    [新增] blynk MQTT 範例

---
+++ homework/config.py
@@ -0,0 +1,7 @@
+# 在此填入 Blynk Device Info 的資訊
+BLYNK_TEMPLATE_ID   = "TMPL6SfB1ZabI"
+BLYNK_TEMPLATE_NAME = "MQTT Demo 1"
+BLYNK_AUTH_TOKEN    = "kZsQTdgK3sZfDgLJCXpTlgo_xysFM_mt"
+
+# 變更預設 Blynk 伺服器。
+BLYNK_MQTT_BROKER   = "blynk.cloud"
