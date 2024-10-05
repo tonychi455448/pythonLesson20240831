91c9f033be4fcad6e24636723493a64ebef87628
tony <tony455448@gmail.com>
2024-10-05 16:17:44 +0800

    [新增] pico sensor 訊息 publish

--- pico/lesson6/main.py
+++ pico/lesson6/main.py
@@ -5,7 +5,8 @@ led->gpio15
 可變電阻->gpio26
 內建溫度sensor->adc(4) adc最後1pin，共5pin
 '''
-
+import time,binascii
+from umqtt.simple import  MQTTClient
 import tools
 from machine import Timer,ADC,Pin,PWM,RTC
 def do_thing(t):
@@ -17,8 +18,10 @@ def do_thing(t):
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
     print(f'溫度:{temperature}')
+    mqtt.publish("SA-58/Temperature",f"{temperature}")
     light_value = adc_light.read_u16()
     print(f'光線:{light_value}')
+    mqtt.publish("SA-58/Light",f"{light_value}")
     
     
 def do_thing1(t):
@@ -31,6 +34,7 @@ def do_thing1(t):
     duty = adc1.read_u16()
     pwm.duty_u16(duty)    
     print(f'可變電阻:{round(duty/65535*10)}')
+    mqtt.publish("SA-58/switch",f"{round(duty/65535*10)}")
     
 def main():
     t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
@@ -50,5 +54,9 @@ if __name__ == "__main__":
         adc = ADC(4) #內建溫度感測器
         adc_light = ADC(Pin(28)) #光線感測器
         pwm = PWM(Pin(15),freq=50) #可變電阻
-        conversion_factor = 3.3 / (65535) #電壓轉換率    
+        conversion_factor = 3.3 / (65535) #電壓轉換率
+        SERVER="192.168.0.252"
+        CLIENT_ID=binascii.hexlify(machine.unique_id())
+        mqtt=MQTTClient(CLIENT_ID,SERVER,user="pi",password="raspberry")
+        mqtt.connect()
         main()
